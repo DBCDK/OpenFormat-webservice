e1ee232b86ca57515e9ed36f99b30138a1c2fa3c
nje <nje@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2013-07-10 11:44:10 +0000

    utilities for requesting a search by pid, creating request_template and null-files i response dirs - invoked by test-script
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/OpenFormat/trunk@60212 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

---
+++ xml/util/reformatNewRequestRecord.py
@@ -0,0 +1,43 @@
+# read the downloadet search result and wrap it in
+# a OpenFormat request template
+# ajust defaut namespace
+
+import re
+import sys
+
+tfilename = sys.argv[1]
+ffilename = sys.argv[2]
+
+t = open(tfilename)
+templatelines = [linet.strip() for linet in t.readlines()]
+t.close();
+
+f = open(ffilename)
+flines = [linef.strip() for linef in f.readlines()]
+f.close()
+
+fundet=False
+for templateline in templatelines:
+  if (templateline.rfind('<os:object') >= 0): fundet=True
+  if (templateline.rfind('<\/os:object') >= 0): fundet=False
+  if (fundet): 
+     tfundet=False
+     fundet=False
+     for fline in flines:
+        if (fline.rfind('<object') >= 0): tfundet=True
+        if (fline.rfind('</object') >= 0): tfundet=False
+        if (tfundet): 
+	  matchObj = re.match(r'<[^:>]*:|</[^:>]*:', fline)
+          if matchObj:
+             print fline
+          else:
+             matchObj = re.match(r'^<[^/]', fline)
+             if matchObj:
+	       res = re.sub(r'^<', "<os:", fline)
+	       res = re.sub(r'</', "</os:", res)
+             else:
+	       res = re.sub(r'</', "</os:", fline)
+             print res 
+  else:
+    templateline = templateline.replace('FILENAMEPLACE', ffilename)
+    print templateline
---
+++ xml/util/search_by_pid.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+## fetch an object by search on pid
+## save the result to filename given by option 2
+
+SERVERNAME=http://lakiseks.dbc.dk/openbibdk/next/
+USAGE="\nusage:\n$0 pid outputfilenavn [search_url]\n\t pid of object\n\t outputfilename where to save the result\n\t search_url for server to request"
+
+if [ "$1"x == "x" ]
+then
+  echo "Mandatory parameter missing: Pid"
+  echo -e $USAGE
+  exit -1 
+fi
+if [ "$2"x == "x" ]
+then
+  echo "Mandatory parameter missing: Output file name"
+  echo -e $USAGE
+  exit -1 
+fi
+if [ "$3"x != "x" ]
+then
+  SERVERNAME=$3
+fi
+
+PID=$1
+SEARCHRESULT=$2
+
+## insert the pid from option 1 into the soap template
+TEMPLATE=post_template.xml
+TMPFILENAME=/tmp/post_template$$.xml
+cat $TEMPLATE | sed -e s/PIDPLACE/$PID/ >$TMPFILENAME
+
+## do the search request
+echo -e "Requesting pid=$PID from $SERVERNAME" >&2
+curl -s  -H "Content-type:text/xml; charset=utf8" -X 'POST' -d @"$TMPFILENAME" $SERVERNAME >$SEARCHRESULT
+rm $TMPFILENAME
+
+## check if we got only one result
+## if zero or more ask the user wheter he wants to quith
+
+HITCOUNT=`grep hitCount <(xmllint -format $SEARCHRESULT)`
+if [ $HITCOUNT != '<hitCount>1</hitCount>' ]
+then
+  head <(xmllint -format $SEARCHRESULT) >&2
+  echo -e "********** WARNING *********" >&2
+  echo -e "*** hitCount less or more than 1 hit!!\n" >&2
+  echo "Quit? [y/n]:"
+  read keyPress
+  if [ $keyPress == 'y' -o $keyPress == 'Y' ]
+  then
+    exit -1
+  fi
+else
+  echo -e "Request result OK, got one object, saved to $SEARCHRESULT" >&2
+fi
+
+## end of script

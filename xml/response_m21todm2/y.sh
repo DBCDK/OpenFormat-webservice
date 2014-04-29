82e76a225f78ab55603b3e2b03f03b82070a9b5e
nje <nje@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-04-29 12:57:49 +0000

    new responses
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/OpenFormat/trunk@71495 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

---
+++ xml/response_m21todm2/y.sh
@@ -0,0 +1,3 @@
+cp /home/nje/OpenFormat/response_m21todm2_tmp/bog_hans_engell_farvel.xml .
+cp /home/nje/OpenFormat/response_m21todm2_tmp/bog_sandburg.xml .
+cp /home/nje/OpenFormat/response_m21todm2_tmp/dm2_hans_engell.xml .
